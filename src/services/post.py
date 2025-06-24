fb826e1d30ca0b297a0688bfe1787a1728ff50d5
Guilherme Sa <guilhermecgsa7@gmail.com>
2025-06-24 08:55:36 -0300

    Atualizando services e routas

--- src/services/post.py
+++ src/services/post.py
@@ -15,50 +15,7 @@ class PostService:
             message = message,
         )
         return new_post
-
-    async def get_post(self, post_id: int):
-        try:
-            return await PostModel.get(id=post_id)
-        except DoesNotExist as e:
-            raise post_notfound()
-
-    async def like_post(self, user_id: int, post_id: int):
-        post = await PostModel.get(id = post_id)
-
-        try:
-            post_like = await PostLikeModel.get(
-                user_id = user_id, 
-                post_id = post_id
-            )
-            await post_like.delete()
-            post.likes_count -= 1
-
-        except DoesNotExist as e:
-            await PostLikeModel.create(
-                user_id = user_id,
-                post_id = post_id,
-            )
-            post.likes_count += 1
-
-        finally:
-            await post.save()
-
-        return post.likes_count
-
-    async def comment_post(self, user_id: int, post_id: int, message: str):
-
-        post = await PostModel.get(id = post_id)
-
-        comment = await PostCommentModel.create(
-            user_id = user_id,
-            post_id = post_id,
-            message = message
-        )
-
-        post.comments_count += 1
-        await post.save()
-
-        return comment
+    
 
     async def get_post_comments(self, post_id: int):
         return await PostCommentModel.filter(post_id = post_id).order_by('-created_at')
